d97392bde8231e07924119a57a577eb0fd798915
indoni <choinj97@gmail.com>
2020-04-26 19:05:07 +0900

    iOS Seminar 2nd-week exercise 2020-04-25

---
+++ iOS_SecondWeek_Seminar_Exercise/iOS_SecondWeek_Seminar_Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_SecondWeek_Seminar_Exercise
+//
+//  Created by 최인정 on 2020/04/25.
+//  Copyright © 2020 indoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
